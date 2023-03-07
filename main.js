4f9bf6ed5975d05b1093391e70fb1c6f8acec580
username <kaimibrahim20@gmail.com>
2023-03-07 08:37:36 -0800

    started with some js

---
+++ main.js
@@ -0,0 +1,29 @@
+/*-------------- Constants ---------------*/
+const questins = [
+  {
+    question: "Where is teh hightest mountain in the world located?",
+    answers: [
+      { text: "Nepal", correct: true },
+      { text: "Denali", correct: false },
+      { text: "Dubai", correct: false },
+      { text: "India", correct: false },
+    ],
+  },
+  {
+    question: "What is the capital of Thailand?",
+    answers: [
+      { text: "Bangkok", correct: true },
+      { text: "Hanoi", correct: false },
+      { text: "Caracas", correct: false },
+      { text: "Bogota", correct: false },
+    ],
+  },
+];
+
+/*-------------------- app's state (variables) ----------- */
+
+/*---------------- cached element ----------------*/
+
+/*---------------- event listeners ------------*/
+
+/*---------------------- function -----------------*/
