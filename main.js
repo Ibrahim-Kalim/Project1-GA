3b616fad712c5effa493083dcefe854c033fc46d
username <kaimibrahim20@gmail.com>
2023-03-08 10:53:27 -0800

    Project1

--- main.js
+++ main.js
@@ -1,51 +1,47 @@
 /*-------------- Constants ---------------*/
-const questions = [
-  {
-    question: "Where is the hightest mountain in the world located?",
-    answers: [
-      "Nepal",
-      "Denali",
-      "Dubai",
-      "India",
-      //   { text: "Nepal", correct: true },
-      //   { text: "Denali", correct: false },
-      //   { text: "Dubai", correct: false },
-      //   { text: "India", correct: false },
-    ],
-  },
+const questions = {
+  question: "Where is the hightest mountain in the world located?",
+  answers: [
+    { text: "Nepal", correct: true },
+    { text: "Denali", correct: false },
+    { text: "Dubai", correct: false },
+    { text: "India", correct: false },
+  ],
+};
 
-  {
-    question: "What is the capital of Thailand?",
-    answers: ["Bangkok", "Hanoi", "Caracas"],
-    // { text: "Bangkok", correct: true },
-    // { text: "Hanoi", correct: false },
-    // { text: "Caracas", correct: false },
-    // { text: "Bogota", correct: false },
-  },
-];
+//   {
+//     question: "What is the capital of Thailand?",
+//     answers: [
+//       { text: "Bangkok", correct: true },
+//       { text: "Hanoi", correct: false },
+//       { text: "Caracas", correct: false },
+//       { text: "Bogota", correct: false },
+//     ],
+//   },
+// ];
 
 /*-------------------- app's state (variables) ----------- */
 
 /*---------------- cached element ----------------*/
 const startButton = document.getElementById("start-btn");
 const questionContainer = document.getElementById("question");
-const answerOptions = document.querySelectorAll("btn");
+const answerOptions = document.querySelectorAll(".btn");
 /*---------------- event listeners ------------*/
-startButton.addEventListener("click", startGame);
+// startButton.addEventListener("click", startGame);
 
 /*---------------------- function -----------------*/
 function startGame() {
-  let nextQuestion = questions[Math.floor(Math.random() * questions.length)];
-  questionContainer.innerHTML = `${nextQuestion.question}`;
-  answerOptions.forEach(function (el, ind) {
-    el.innerHTML = questions.answers[ind];
-    answerOptions.innerHTML = `${answers}`;
+  questionContainer.innerHTML = `${questions.question}`;
+  let answerList = questions.answers.forEach(function (el, ind) {
+    answerOptions[ind].innerHTML = el.text;
+    console.log(answerOptions[ind].innerHTML);
+    console.log(el.text);
   });
-
-  //   setNextQuestion();
 }
+
 startGame();
+
+//   setNextQuestion();
 
 function setNextQuestion() {}
 setNextQuestion();
-console.log(answerOptions);
