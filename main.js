3f60646f70f30af551030b7734b9528432589461
username <kaimibrahim20@gmail.com>
2023-03-08 09:40:09 -0800

    Project1

--- main.js
+++ main.js
@@ -1,29 +1,51 @@
 /*-------------- Constants ---------------*/
-const questins = [
+const questions = [
   {
-    question: "Where is teh hightest mountain in the world located?",
+    question: "Where is the hightest mountain in the world located?",
     answers: [
-      { text: "Nepal", correct: true },
-      { text: "Denali", correct: false },
-      { text: "Dubai", correct: false },
-      { text: "India", correct: false },
+      "Nepal",
+      "Denali",
+      "Dubai",
+      "India",
+      //   { text: "Nepal", correct: true },
+      //   { text: "Denali", correct: false },
+      //   { text: "Dubai", correct: false },
+      //   { text: "India", correct: false },
     ],
   },
+
   {
     question: "What is the capital of Thailand?",
-    answers: [
-      { text: "Bangkok", correct: true },
-      { text: "Hanoi", correct: false },
-      { text: "Caracas", correct: false },
-      { text: "Bogota", correct: false },
-    ],
+    answers: ["Bangkok", "Hanoi", "Caracas"],
+    // { text: "Bangkok", correct: true },
+    // { text: "Hanoi", correct: false },
+    // { text: "Caracas", correct: false },
+    // { text: "Bogota", correct: false },
   },
 ];
 
 /*-------------------- app's state (variables) ----------- */
 
 /*---------------- cached element ----------------*/
-
+const startButton = document.getElementById("start-btn");
+const questionContainer = document.getElementById("question");
+const answerOptions = document.querySelectorAll("btn");
 /*---------------- event listeners ------------*/
+startButton.addEventListener("click", startGame);
 
 /*---------------------- function -----------------*/
+function startGame() {
+  let nextQuestion = questions[Math.floor(Math.random() * questions.length)];
+  questionContainer.innerHTML = `${nextQuestion.question}`;
+  answerOptions.forEach(function (el, ind) {
+    el.innerHTML = questions.answers[ind];
+    answerOptions.innerHTML = `${answers}`;
+  });
+
+  //   setNextQuestion();
+}
+startGame();
+
+function setNextQuestion() {}
+setNextQuestion();
+console.log(answerOptions);
