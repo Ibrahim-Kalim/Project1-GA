4450438cce461a970eb64a353b0584e68e685a3c
username <kaimibrahim20@gmail.com>
2023-03-09 09:13:25 -0800

    project1

--- main.js
+++ main.js
@@ -34,22 +34,22 @@ const d_text = document.getElementById("d_text");
 const btn = document.getElementById("btn"); // got the button by its id
 
 /*---------------- event listeners ------------*/
+btn.addEventListener("click", ())
 
 /*---------------------- function -----------------*/
 // created function to load question
 function loadQuizDataAndStart() {
   if (currentQuiz < quizData.length) {
-    const currentQuizInfo = quizData[currentQuiz];
-    question.innerHTML = currentQuizInfo.question;
-    question.innerText = currentQuizInfo.question;
-    a_text.innerText = currentQuizInfo.a;
-    b_text.innerText = currentQuizInfo.b;
-    c_text.innerText = currentQuizInfo.c;
-    d_text.innerText = currentQuizInfo.d;
+    const currentQuizInfo = quizData[currentQuiz]; // to start with the question 1 from object quesData
+    question.innerText = currentQuizInfo.question; // to display the question from the object and replace text frm quez to the id question
+    a_text.innerText = currentQuizInfo.a; // display the choice from object to inside html li chice
+    b_text.innerText = currentQuizInfo.b; // display the choice from object to inside html li chice
+    c_text.innerText = currentQuizInfo.c; // display the choice from object to inside html li chice
+    d_text.innerText = currentQuizInfo.d; // display the choice from object to inside html li chice
   } else {
     quiz.innerHTML = `
   <h3 class= "score_text">You have answered ${score}/${quizData.length}</h3>
   <button class="btn_btn_start" onClick="location.reload()">Restart</button>`;
   }
-};
-
+}
+loadQuizDataAndStart();
