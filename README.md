2f04c800a96a08d9398e3f60601f6093c100e88b
username <kaimibrahim20@gmail.com>
2023-03-10 09:55:44 -0800

    taking off all commented

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 My game is going to be question game with 4 questins with four possible answers. the game with display question on the top and the 4 answers for the player to chose from. On the bottom with see the submit button that will take to the next question as well as recoreds the score. In HTMl will include first div with the ques and another div that hold all the anwsers, and last div to hold the button.
 
-![The first page of the Game](/imag/theFirstPage.jpeg.png)
+[The first page of the Game](/imag/theFirstPage.jpeg.png)
 The HTML will gave the question as h2 tagElement and all the choices will be hold inside Ul unordered lists.
 
 Javascript will have most of the work. I created a object called quizData that hold all the three quistions with the four answers under each quiz. The main function of the game is the loadQuizDataAndStart() wich display the game and guid us through the next quiz.
@@ -10,6 +10,6 @@ The next important function of the game is getCheckedAnswer() that help us by lo
 Another use of the getCheckedAnswer() is through teh addEventListner since we're using only one button to navigate throguh the game. Moreover is the another if condition that add the score++ after each correct answer and then add more quiz to the currentQuiz++.
 
 The quiz game is my first ever game that shows me the important of the eventLister and the callback function. Eventhough is not totally where it should be, but I'm totally satisfied by the results and more to learn and apply.
-![The last page of the Game](/imag/the%20lastPageOfTheGame.jpeg.png)
+[The last page of the Game](/imag/the%20lastPageOfTheGame.jpeg.png)
 
 https://ibrahim-kalim.github.io/Project1-GA/
--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-/*-------------- Constants ---------------*/
-//created object that have all question
 const quizData = [
   {
     question: "In which Italian city can you find the Colosseum?",
@@ -27,43 +25,37 @@ const quizData = [
   },
 ];
 
-/*-------------------- app's state (variables) ----------- */
-let currentQuiz = 0; // to follow which quiz am I
-let score = 0; // to start score by 0 and later to add up
+let currentQuiz = 0;
+let score = 0;
 let answer = 0;
 
-/*---------------- cached element ----------------*/
-const quiz = document.getElementById("quiz"); //get the id queiz that hold the question
-const answerLi = document.querySelectorAll(".answer"); // got all answer by queryselector
-const question = document.getElementById("question"); // got the h2 by its id
-const a_text = document.getElementById("a_text"); // get the choice li by the id
+const quiz = document.getElementById("quiz");
+const answerLi = document.querySelectorAll(".answer");
+const question = document.getElementById("question");
+const a_text = document.getElementById("a_text"); //
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
-const btn = document.getElementById("btn"); // got the button by its id
+const btn = document.getElementById("btn");
 
-/*---------------- event listeners ------------*/
 btn.addEventListener("click", function () {
-  answer = getCheckedAnswer(); // answer is a funtion to get the answer thorugh forEch loop in order to the checked answer to compare with the corect answer
-  console.log(answer);
-  if (answer === quizData[currentQuiz].correct) score++; // to add to score varible 1 each time you answer the quez
-  currentQuiz++; // add one queiz each time to hit submit
+  answer = getCheckedAnswer();
 
-  loadQuizDataAndStart(); // added loadQuiz function in order to load the next quiz
+  if (answer === quizData[currentQuiz].correct) score++;
+  currentQuiz++;
+
+  loadQuizDataAndStart();
   deSelect();
 });
 
-/*---------------------- function -----------------*/
-// created function to load question
 function loadQuizDataAndStart() {
   if (currentQuiz < quizData.length) {
-    // to compar to finish up all question to display restart button {
-    const currentQuizInfo = quizData[currentQuiz]; // to start with the question 1 from object quesData
-    question.innerText = currentQuizInfo.question; // to display the question from the object and replace text frm quez to the id question
-    a_text.innerText = currentQuizInfo.a; // display the choice from object to inside html li chice
-    b_text.innerText = currentQuizInfo.b; // display the choice from object to inside html li chice
-    c_text.innerText = currentQuizInfo.c; // display the choice from object to inside html li chice
-    d_text.innerText = currentQuizInfo.d; // display the choice from object to inside html li chice
+    const currentQuizInfo = quizData[currentQuiz];
+    question.innerText = currentQuizInfo.question;
+    a_text.innerText = currentQuizInfo.a;
+    b_text.innerText = currentQuizInfo.b;
+    c_text.innerText = currentQuizInfo.c;
+    d_text.innerText = currentQuizInfo.d;
   } else {
     quiz.innerHTML = `
   <h3 class= "score_text">You have answered ${score}/${quizData.length}</h3>
@@ -78,9 +70,7 @@ function deSelect() {
   });
 }
 
-// created function to ckeck the answer to check whic choices the player chose only by looping through all the choices
 function getCheckedAnswer() {
-  // usging the answerLi that catched all the answers (li elemnt) choices from choices
   answerLi.forEach((el) => {
     if (el.checked) {
       answer = el.id;
